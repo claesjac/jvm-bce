7520b5e9d24601e2e392e7a8753fe125a34df3f5
Claes Jakobsson <claes@surfar.nu>
2011-09-13 21:17:16 +0200

    Project setup

---
+++ lib/JVM/BCE.pm
@@ -0,0 +1,21 @@
+package JVM::BCE;
+
+use strict;
+use warning;
+
+our $VERSION = "0.01";
+
+1;
+=pod
+
+=head1 NAME
+
+JVM::BCE - JVM ByteCode (Engineering | Experimentation | Excitement)
+
+=head1 DESCRIPTION
+
+This is a module for reading, manipulating, generating, writing JVM bytecode (and class files).
+
+=cut
+
+
